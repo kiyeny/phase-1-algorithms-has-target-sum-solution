a99619a54a09da7c372cc32e24c8c91ba109cb3e
maxwell <maxwell.kiyeny@student.moringaschool.com>
2022-06-20 14:58:24 +0300

    my index.js commit

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  for (let i = 0; i < array.length; i++) {
+    const noToBeAdded = target - array[ i ];
+    if (myNumbObject[ noToBeAdded ]) return true;
+    myNumbObject[ array[ i ] ] = true;
+  }
+  return false;
 }
+
+
+  function hasTargetSum(array, target) {
+    const myNumbObject = {} 
+
+    for (const number of array) { 
+      const noToBeAdded = target - number;
+      if (myNumbObject[ noToBeAdded ]) return true;
+      myNumbObject[number] = true;
+    }
+    return false;
+  }
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
-
 /* 
   Add your pseudocode here
 */
-
 /*
   Add written explanation of your solution here
 */
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
   console.log("");
-
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
-
 module.exports = hasTargetSum;
